1d99535ffbe7f6da4bc2271b4f1b542318fb5270
aubrey whitford <aubreya.whitford@gmail.com>
2024-11-14 12:07:25 -0500

    Add Search History Heading

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
 const baseUrl = "https://www.thecolorapi.com/id?hex=24B1E0";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     function fetchColor() { // PROJECT //
         fetch(baseUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log("Color Data", data);
-            const colors = data
-            const colorDiv = document.getElementById("color-div");
-            
-            const colorImage = document.createElement('img');
-            colorImage.src = colors.image.bare;
-            colorDiv.appendChild(colorImage)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Color Data", data);
+                const colors = data
+                const colorDiv = document.getElementById("color-div");
 
-            const colorInfo = document.createElement("p");
-            colorInfo.textContent = `${colors.name.value} - ${colors.hex.value}` || "Unknown Color"
-            colorDiv.appendChild(colorInfo)
+                const colorImage = document.createElement('img');
+                colorImage.src = colors.image.bare;
+                colorDiv.appendChild(colorImage)
 
-            const hexForm = document.createElement("form");
-            hexForm.id = "hex-form";
-            colorDiv.appendChild(hexForm);
+                const colorInfo = document.createElement("p");
+                colorInfo.textContent = `${colors.name.value} - ${colors.hex.value}` || "Unknown Color"
+                colorDiv.appendChild(colorInfo)
 
-            const hexInput = document.createElement("input");
-            hexInput.type = "text";
-            hexInput.id = "hex-input";
-            hexInput.placeholder = "Enter HEX Code"
-            hexForm.appendChild(hexInput);
+                const hexForm = document.createElement("form");
+                hexForm.id = "hex-form";
+                colorDiv.appendChild(hexForm);
 
-            const submitButton = document.createElement("button");
-            submitButton.type = ("submit");
-            submitButton.textContent = ("Submit");
-            hexForm.appendChild(submitButton);
+                const hexInput = document.createElement("input");
+                hexInput.type = "text";
+                hexInput.id = "hex-input";
+                hexInput.placeholder = "Enter HEX Code"
+                hexForm.appendChild(hexInput);
 
-            hexForm.addEventListener("submit", (event) => {
-                event.preventDefault();
-                const input = hexInput.value;
-                console.log(input);
+                const submitButton = document.createElement("button");
+                submitButton.type = ("submit");
+                submitButton.textContent = ("Submit");
+                hexForm.appendChild(submitButton);
 
-                hexForm.reset();
+                const historyHeading = document.createElement("h3")
+                historyHeading.id = "history-heading"
+                historyHeading.textContent = "Search History"
+                colorDiv.appendChild(historyHeading)
+
+                hexForm.addEventListener("submit", (event) => {
+                    event.preventDefault();
+                    const input = hexInput.value;
+                    console.log(input);
+
+                    hexForm.reset();
+                })
+
+
             })
-
-
-        })
-        .catch(error => console.error("Color fetch error:", error));
+            .catch(error => console.error("Color fetch error:", error));
     }
 
     fetchColor();
